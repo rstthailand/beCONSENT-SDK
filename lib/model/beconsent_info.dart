1220255eff75c4bb4d0167c829f85df84518a9aa
RealRavip <real862545@gmail.com>
2022-07-18 11:42:39 +0700

    as

--- lib/model/beconsent_info.dart
+++ lib/model/beconsent_info.dart
@@ -1,15 +1,5 @@
 import 'package:http/http.dart' as http;
-import 'package:beconsent_sdk/model/getWorkspace.dart';
 
-
-
-Future <GetWorkspace> getData(GetWorkspace _ws) async{
-    final url = Uri.parse("http://sit-consent.beconsent.tech:3003/api/v1/workspaces/1");
-    var response = await http.get(url);
-    print(response.body);
-    _ws = getWorkspaceFromJson(response.body);
-    return _ws;
-  }
 
 cancelConsent() async{
     final url = Uri.parse("http://dev.beconsent.tech/api/v1/03a29a62-eb39-4d7b-895c-7e900d893e37/user-consents");
---
+++ lib/model/record_consent.dart
@@ -0,0 +1,15 @@
+class consent_record{
+  int id;
+  String uuid;
+  bool val;
+  String name;
+  String description;
+
+  consent_record({
+    required this.id,
+    required this.uuid,
+    required this.val,
+    required this.name,
+    required this.description
+});
+}
