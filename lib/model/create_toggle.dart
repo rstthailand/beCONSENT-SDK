64e204ad010c55a43e06b8ba75d3f405ae9adea4
RealRavip <real862545@gmail.com>
2022-07-18 11:45:23 +0700

    as

--- lib/model/create_toggle.dart
+++ lib/model/create_toggle.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:beconsent_sdk/model/toggle_switch.dart';
 import 'package:beconsent_sdk/model/Consent.dart';
+import 'package:beconsent_sdk/model/record_consent.dart';
 
 class create_toggle extends StatefulWidget {
   late Consent _c;
@@ -21,20 +22,37 @@ class _create_toggleState extends State<create_toggle> {
   }
 
   late Consent _c;
+  List<consent_record> record = [];
   var consent = [];
   var description = [];
   late String lang = _c.defaultLanguage;
   bool val = false;
 
   add_index() {
-    for (var i in _c.purposes) {
-      for (var k in i.purposeCategories) {
-        if (lang == 'th') {
-          consent.add(k.name.th);
-          description.add(k.description.th);
-        } else {
-          consent.add(k.name.en);
-          description.add(k.description.en);
+    if (!record.isEmpty) {
+      for (var i in record) {
+        print('${i.val} ${i.name} ');
+      }
+    } else {
+      for (var i in _c.purposes) {
+        for (var k in i.purposeCategories) {
+          if (lang == 'th') {
+            consent_record rec = consent_record(
+                id: k.id,
+                uuid: k.uuid,
+                val: val,
+                name: k.name.th,
+                description: k.description.th);
+            record.add(rec);
+          } else {
+            consent_record rec = consent_record(
+                id: k.id,
+                uuid: k.uuid,
+                val: val,
+                name: k.name.en,
+                description: k.description.en);
+            record.add(rec);
+          }
         }
       }
     }
@@ -46,9 +64,24 @@ class _create_toggleState extends State<create_toggle> {
     return ListView.builder(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
-        itemCount: consent.length,
+        itemCount: record.length,
         itemBuilder: (context, i) {
-          return toggle_switch(consent[i],description[i]);
+          return Card(
+            child: ListTile(
+              title: Text(record[i].name,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+              subtitle: Text(record[i].description),
+              trailing: CupertinoSwitch(
+                  value: record[i].val,
+                  onChanged: (newValue) {
+                    setState(() {
+                      record[i].val = newValue;
+                    });
+                  },
+                  trackColor: Colors.grey,
+                  activeColor: Colors.blue),
+            ),
+          );
         });
   }
 }
