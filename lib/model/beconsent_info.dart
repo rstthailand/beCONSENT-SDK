e669d2c624f67df7480b9f0058ca8ee712f6654a
RealRavip <real862545@gmail.com>
2022-07-25 09:54:39 +0700

    as

--- lib/model/beconsent_info.dart
+++ lib/model/beconsent_info.dart
@@ -7,7 +7,35 @@ import 'package:beconsent_sdk/model/globals.dart' as global;
 cancelConsent(Consent _c) async {
   final url = Uri.parse(
       "http://dev.beconsent.tech/api/v1/03a29a62-eb39-4d7b-895c-7e900d893e37/user-consents");
-  List<int> purpose = [];
+  final user_check = Uri.parse(
+      "http://dev.beconsent.tech/api/v1/${_c.workspaceUUID}/user-consents/consent/${_c.consentUUID}/version/${_c.version}/get-user-consent");
+  var res = await http.post(user_check, body: {"uid": global.uid});
+  if(res.statusCode == 201){
+    List<int> purpose = [];
+  for (var i in global.record) {
+    if (i.primary == true) {
+      global.Action = "CHANGE_TO_PARTIAL";
+      purpose.add(i.id);
+    }
+  }
+  Map<String, dynamic> args = {
+    "consentId": _c.consentId.toString(),
+    "uid": global.uid,
+    "name": global.Name,
+    "consentVersion": _c.version,
+    "purposes": purpose,
+    "action": global.Action,
+    "language": "th",
+    "collectionChannel": "Mobile App"
+  };
+  var body = json.encode(args);
+  var response = await http
+      .post(url, body: body, headers: {'Content-type': 'application/json'});
+  print(response.statusCode);
+  print(response.body);
+  }
+  if(res.statusCode == 404){
+    List<int> purpose = [];
   for (var i in global.record) {
     if (i.primary == true) {
       global.Action = "PARTIAL";
@@ -29,6 +57,8 @@ cancelConsent(Consent _c) async {
       .post(url, body: body, headers: {'Content-type': 'application/json'});
   print(response.statusCode);
   print(response.body);
+  }
+  
 }
 
 saveConsent(Consent _c) async {
@@ -37,7 +67,6 @@ saveConsent(Consent _c) async {
   final user_check = Uri.parse(
       "http://dev.beconsent.tech/api/v1/${_c.workspaceUUID}/user-consents/consent/${_c.consentUUID}/version/${_c.version}/get-user-consent");
   var res = await http.post(user_check, body: {"uid": global.uid});
-  print(res.statusCode);
 
   if (res.statusCode == 404) {
     List<int> purpose = [];
@@ -70,7 +99,7 @@ saveConsent(Consent _c) async {
         global.Action = "CHANGE_TO_PARTIAL";
         purpose.add(i.id);
       }
-      if(global.accept_all = true){
+      if (global.accept_all = true) {
         global.Action = "CHANGE_TO_ALL";
         purpose.add(i.id);
       }
